b687504068e7e90fc310ba99d6a800f1ba200584
Maryam-Inam <maryaminam00@gmail.com>
2022-02-17 07:21:42 +0500

    node_modules reintallation

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App'
 import { StateProvider } from './components/StateProvider'
 
-ReactDOM.render( < App / > , document.getElementById('root')) {
-    /* <StateProvider initialState={initialState} reducer={reducer}>
-</StateProvider> */
-}
+ReactDOM.render( < App / > , document.getElementById('root'));
+
+    {/* <StateProvider initialState={initialState} reducer={reducer}>
+</StateProvider>  */}
